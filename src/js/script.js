f875976023b97cc89adfa49c944f2247ae3ed225
Ulvi Shabanbayli <ulvi.shabanov@gmail.com>
2024-06-03 18:10:41 +0400

    second commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,18 @@
+"use strict";
+/*
+// 1.
+let a = 100;
+if (a % 33 > 30 && (a % 33) % 14 > 10) console.log(((a % 33) % 14) % 5);
 
+// 2.
+let x = Number(prompt("Eni daxil edir"));
+let y = Number(prompt("Uzunlugu daxil edir"));
+
+if (x > 10 && y > 10) console.log(x * y);
+if (x < 10 && y < 10) console.log(2 * (x + y));
+
+// 3.
+let z = Number(prompt());
+let area = z ** 2;
+if (area > 25) console.log(area % 12);
+*/
